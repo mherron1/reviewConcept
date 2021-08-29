b25d8194dd0bf02e0f51d78727a29a2ba5c58923
mherron1 <mherron2021@gmail.com>
2021-08-29 04:02:20 +0100

    initial

---
+++ index.js
@@ -0,0 +1,40 @@
+const googlesheetsID = "1evyjvVPsZWZI4VTV1P_4DDzbW4rUbK2HGXVUJQ_3iUA"
+
+const review_page_container = document.querySelector("#container")
+
+//https://docs.google.com/spreadsheets/d/1evyjvVPsZWZI4VTV1P_4DDzbW4rUbK2HGXVUJQ_3iUA/edit?usp=sharing
+
+
+const url = `https://docs.google.com/spreadsheets/d/${googlesheetsID}/gviz/tq?`;
+
+const reviewArray = []
+
+fetch(url)
+.then(res =>res.text())
+.then (data =>{
+    const json = JSON.parse(data.substr(47).slice(0, -2))
+   json.table.rows.forEach(row=>{
+           reviewArray.push({
+               name: row.c[1].v,
+               email: row.c[2].v,
+               phone: row.c[3].v,
+               message: row.c[4].v,
+               review_image: row.c[5].v 
+           })
+   })  
+   
+   reviewArray.forEach(review=>{
+
+    review_page_container.innerHTML +=  `
+       <div class="box">
+        <h4>${review.name}</h4>
+        <p>${review.email}</p>
+        <p>${review.phone}</p>
+        <p>${review.message}</p>
+        <img style="height:175px; width:175px;" src="${review.review_image}" />
+       </div>
+  `
+})
+})
+
+
